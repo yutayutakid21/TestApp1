a9aa60a4083241efcee5c12f1a9bf3ba66bfe21f
Yuta Fujii <yutayutakid21@gmail.com>
2020-06-30 16:12:31 +0900

    課題

--- TestApp1/ViewController.swift
+++ TestApp1/ViewController.swift
@@ -10,11 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    /*
+     
+     課題:下記のLabel3つの背景色を赤から青に変更し、テキストを好きなテキストへ変更し、masterへpushしてください。
+     
+     */
+    
+    var label1 = UILabel()
+    var label2 = UILabel()
+    var label3 = UILabel()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        label1.text = "テスト1"
+        label2.text = "テスト2"
+        label3.text = "テスト3"
+    
+        label1.textColor = .white
+        label2.textColor = .white
+        label3.textColor = .white
+        
+        label1.backgroundColor = .red
+        label2.backgroundColor = .red
+        label3.backgroundColor = .red
+        
+        label1.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height/3)
+        label2.frame = CGRect(x: 0, y: view.frame.size.height/3, width: view.frame.size.width, height: view.frame.size.height/3)
+        label2.frame = CGRect(x: 0, y: label2.frame.origin.y, width: view.frame.size.width, height: view.frame.size.height/3)
+        
+        view.addSubview(label1)
+        view.addSubview(label2)
+        view.addSubview(label3)
         // Do any additional setup after loading the view.
     }
+    
+    
 
-
+    
+    
+    
 }
 
